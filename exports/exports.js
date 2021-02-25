bdd06924e64f436ca91b32295c73c32098ddd71f
Jystro <35638179+Jystro@users.noreply.github.com>
2021-02-25 20:40:09 +0100

    Added authorised role functions
    Added function to manage admin roles

--- exports/exports.js
+++ exports/exports.js
@@ -5,7 +5,10 @@ module.exports = {
     getPrefix(id) {
         try {
             let servers = JSON.parse(fs.readFileSync('./servers.json'));
-            return servers[id].prefix.value;
+            if(typeof(servers[id].prefix.value) !== 'undefined') {
+                return servers[id].prefix.value;
+            }
+            return defaultPrefix;
         }
         catch {
             return defaultPrefix;
@@ -15,14 +18,82 @@ module.exports = {
         return new Promise((resolve, reject) => {
             try {
                 let servers = JSON.parse(fs.readFileSync('./servers.json'));
-                if(!servers[id]) { servers[id] = {} }
-                if(!servers.hasOwnProperty('prefix')) { servers[id].prefix = {} }
+                if(!servers.hasOwnProperty(id)) { servers[id] = {} }
+                if(!servers[id].hasOwnProperty('prefix')) { servers[id].prefix = {} }
                 servers[id].prefix.value = prefix;
                 fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 4));
-                resolve('Successfully changed the prefix. New prefix: ' + prefix);
+                resolve({ message: 'Successfully changed the prefix. New prefix: ' + prefix });
             }
             catch(err) {
-                reject(err);
+                reject({ message: 'There was an error while trying to change prefix. Please check the github page to open an issue or join the Discord server', error: err });
+            }
+        });
+    },
+    getRoles(id) {
+        try {
+            let servers = JSON.parse(fs.readFileSync('./servers.json'));
+            if(typeof(servers[id].prefix.roles) !== 'undefined') {
+                return servers[id].prefix.roles;
+            }
+            return [];
+        }
+        catch {
+            return [];
+        }
+    },
+    addRole(id, roles) {
+        return new Promise((resolve, reject) => {
+            try {
+                let servers = JSON.parse(fs.readFileSync('./servers.json'));
+                //check that the property roles exists
+                if(!servers.hasOwnProperty(id)) { servers[id] = {} }
+                if(!servers[id].hasOwnProperty('prefix')) { servers[id].prefix = {} }
+                if(!servers[id].prefix.hasOwnProperty('roles')) { servers[id].prefix.roles = [] }
+
+                roles.forEach(role => {
+                    //correctly format role
+                    if(role.indexOf('<') >= 0) {
+                        role = role.slice(3, -1);
+                    }
+                    //check if the role to add doesn't exists
+                    if(servers[id].prefix.roles.indexOf(role) < 0) {
+                        servers[id].prefix.roles.push(role);
+                    }
+                });
+
+                fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 4));
+                resolve({ message: 'Successfully updated the authorised roles' });
+            }
+            catch(err) {
+                reject({ message: 'There was an error while trying to change authorised roles. Please check the github page to open an issue or join the Discord server', error: err })
+            }
+        });
+    },
+    removeRole(id, roles) {
+        return new Promise((resolve, reject) => {
+            try {
+                let servers = JSON.parse(fs.readFileSync('./servers.json'));
+                //check that the property roles exists
+                if(!servers[id]) { servers[id] = {} }
+                if(!servers[id].hasOwnProperty('prefix')) { servers[id].prefix = {} }
+                if(!servers[id].prefix.hasOwnProperty('roles')) { servers[id].prefix.roles = [] }
+
+                roles.forEach(role => {
+                    //correctly format role
+                    if(role.indexOf('<') >= 0) {
+                        role = role.slice(3, -1);
+                    }
+                    //check if the role to delete exists
+                    if(servers[id].prefix.roles.indexOf(role) >= 0) {
+                        servers[id].prefix.roles.splice(servers[id].prefix.roles.indexOf(role), 1);
+                    }
+                });
+
+                fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 4));
+                resolve({ message: 'Successfully updated the authorised roles' });
+            }
+            catch(err) {
+                reject({ message: 'There was an error while trying to change authorised roles. Please check the github page to open an issue or join the Discord server', error: err })
             }
         });
     }
