24f4ea16934ab8a98441195858c6cd29e11eca37
Jystro <35638179+Jystro@users.noreply.github.com>
2021-08-01 12:59:37 +0200

    Moved variable declaration for faster response time

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'ping',
     description: 'Bot\'s response time. Commands that rely on other websites may take more time',
     execute(message) {
-        const pingTime = message.createdTimestamp;
         message.channel.send('Pong!')
             .then(response => {
+                const pingTime = message.createdTimestamp;
                 const pongTime = response.createdTimestamp;
                 const ping = pongTime - pingTime;
                 message.channel.send('Response time: ' + ping + 'ms')
